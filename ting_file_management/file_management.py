7dae9d63ab984968a304ed94b9a515e6747646eb
Daniel Oliveira <danoliveira865@gmail.com>
2023-09-01 18:41:26 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+        return None
+    try:
+        with open(path_file, "r") as file:
+            file_data = file.read()
+            return file_data.split('\n')
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
