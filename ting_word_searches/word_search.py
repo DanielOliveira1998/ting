0ca45005a717fc1bfb03a7dc45f8f5343f930913
Daniel Oliveira <danoliveira865@gmail.com>
2023-09-05 00:46:47 -0300

    req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_result = []
+    for i in range(len(instance)):
+        file_data = instance.search(i)
+        occurrence_lines = []
+        lines = []
+        for item in file_data["linhas_do_arquivo"]:
+            lines.extend(item.splitlines())
+        occurrence_lines = [{
+            "linha": number}
+            for number, line in enumerate(lines, start=1)
+            if word.lower() in line.lower()]
+        if occurrence_lines:
+            search_result.append({
+                "palavra": word,
+                "arquivo": file_data["nome_do_arquivo"],
+                "ocorrencias": occurrence_lines,
+            })
+    return search_result
 
 
 def search_by_word(word, instance):
